ea2ce87b20547cd5058493d56a131408b9791345
Chet-ok <65378320+Chet-ok@users.noreply.github.com>
2023-11-18 17:09:24 +0800

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,2 +1,12 @@
+def main():
+    password = get_password()
+    censor_password(password)
+
+
 def censor_password(password):
-    print("*"*len(password))
+    print("*" * len(password))
+
+
+def get_password():
+    password = input("Enter password: ")
+    return password
