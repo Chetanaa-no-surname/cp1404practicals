b4bdca86cb633054b80044f8d1feb7a50e0f5a8a
Chet-ok <65378320+Chet-ok@users.noreply.github.com>
2023-11-20 16:52:26 +0800

    Completed the 4 lines of task in prac

---
+++ prac_03/randoms.py
@@ -0,0 +1,17 @@
+# Line 1:
+# Smallest: 5
+# Largest: 20
+
+# Line 2:
+# Smallest:3
+# Largest: 9
+# This cannot produce a 4 since with the skip, the possible numbers will
+# only be [3,5,7,9]. There will be no even numbers like 4
+
+# Line 3
+# Smallest: 2.5
+# Largest: 5.5
+
+# Line 4:
+import random
+print(random.randint(1,100))
