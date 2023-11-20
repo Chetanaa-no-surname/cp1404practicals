e621bd7aa1be72df844fa5d49897d4fa1cc2c8c0
Chet-ok <65378320+Chet-ok@users.noreply.github.com>
2023-11-20 18:03:09 +0800

    Completed part 2

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,9 +1,18 @@
-#Part 1.
+# Part 1.
 OUT_FILE = "name.txt"
-out_file = open(OUT_FILE, "a")
+out_file = open(OUT_FILE, "w")
 
-name = input("Enter name: ")
-out_file.write(name+"\n")
+name = input("Enter name: ").title()
+out_file.write(name + "\n")
+
+out_file.close()
+
+# Part 2.
+OUT_FILE = "name.txt"
+out_file = open(OUT_FILE,"r")
+
+name = out_file.readline()
+print(f"Your name is {name}")
 
 out_file.close()
 
