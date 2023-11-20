dc4bb35b9c6528efb8d50c6871cfbde912d311a2
Chet-ok <65378320+Chet-ok@users.noreply.github.com>
2023-11-20 18:14:23 +0800

    Completed part 3

--- prac_03/files.py
+++ prac_03/files.py
@@ -9,12 +9,21 @@ out_file.close()
 
 # Part 2.
 OUT_FILE = "name.txt"
-out_file = open(OUT_FILE,"r")
+out_file = open(OUT_FILE, "r")
 
 name = out_file.readline()
 print(f"Your name is {name}")
 
 out_file.close()
 
+# Part 3.
+FILENAME = "numbers"
+out_file = open(FILENAME, "r")
 
-
+total = 0
+line_number = 0
+for line in out_file:
+    if line_number < 2:
+        total += int(line)
+    line_number += 1
+print(f"Result is {total}")
