828290faa9b160893d45ef7ea24f798f04e909af
Chet-ok <65378320+Chet-ok@users.noreply.github.com>
2023-11-27 12:52:06 +0800

    Completed Question

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -3,8 +3,8 @@ CP1404/CP5632 - Practical
 Password checker "skeleton" code to help you get started
 """
 
-MIN_LENGTH = 2
-MAX_LENGTH = 6
+MIN_LENGTH = 5  # Changed upper and lower length to match example
+MAX_LENGTH = 15
 SPECIAL_CHARS_REQUIRED = False
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
@@ -28,23 +28,36 @@ def main():
 
 def is_valid_password(password):
     """Determine if the provided password is valid."""
-    # TODO: if length is wrong, return False
+    # if length is wrong, return False
+    if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
+        return False
+    else:
+        count_lower = 0
+        count_upper = 0
+        count_digit = 0
+        count_special = 0
+        for char in password:
+            # count each kind of character (use str methods like isdigit)
+            if char.islower():
+                count_lower += 1
+            elif char.isupper():
+                count_upper += 1
+            elif char.isdigit():
+                count_digit += 1
+            elif char in SPECIAL_CHARACTERS:
+                count_special += 1
 
-    count_lower = 0
-    count_upper = 0
-    count_digit = 0
-    count_special = 0
-    for char in password:
-        # TODO: count each kind of character (use str methods like isdigit)
-        pass
+        # if any of the 'normal' counts are zero, return False
+        if count_lower == 0 or count_upper == 0 or count_digit == 0:
+            return False
 
-    # TODO: if any of the 'normal' counts are zero, return False
+        # if special characters are required, then check the count of those
+        if SPECIAL_CHARS_REQUIRED == True and count_special == 0:
+            return False
+        # and return False if it's zero
 
-    # TODO: if special characters are required, then check the count of those
-    # and return False if it's zero
-
-    # if we get here (without returning False), then the password must be valid
-    return True
+        # if we get here (without returning False), then the password must be valid
+        return True
 
 
 main()
