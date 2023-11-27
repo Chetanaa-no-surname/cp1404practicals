32f2f11c86b34df03b89d8080172d6907f6fc4f8
Chet-ok <65378320+Chet-ok@users.noreply.github.com>
2023-11-27 17:28:19 +0800

    Completed code, changed num to numebr

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -6,25 +6,25 @@ Starter code for cumulative total income program
 
 def main():
     """Display income report for incomes over a given number of months."""
-    num_of_months = int(input("How many months? "))
+    number_of_months = int(input("How many months? "))
 
-    incomes = get_incomes(num_of_months)
+    incomes = get_incomes(number_of_months)
 
-    print_report(incomes, num_of_months)
+    print_report(incomes, number_of_months)
 
 
-def get_incomes(num_of_months):
+def get_incomes(number_of_months):
     incomes = []
-    for month in range(1, num_of_months + 1):
+    for month in range(1, number_of_months + 1):
         income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
     return incomes
 
 
-def print_report(incomes, num_of_months):
+def print_report(incomes, number_of_months):
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, num_of_months + 1):
+    for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
