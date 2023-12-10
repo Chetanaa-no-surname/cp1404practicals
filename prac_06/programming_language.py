38ab5719881e9d27c0605745107fc54d468f97b8
Chet-ok <65378320+Chet-ok@users.noreply.github.com>
2023-12-10 19:57:40 +0800

    Added basic initialization and dynamic check

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -6,9 +6,10 @@ Estimated Time: 30 mins
 Actual Time:
 """
 
+
 class ProgrammingLanguage:
 
-    def __int__(self,name,typing,reflection,year):
+    def __int__(self, name="", typing="", reflection="", year=0):
         self.name = name.title()
         self.typing = typing.title()
         self.reflection = reflection
@@ -19,6 +20,3 @@ class ProgrammingLanguage:
             return True
         else:
             return False
-
-
-
