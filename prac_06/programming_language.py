8fb4aa61af74fd7f8e256eb0c54a61239f71449c
Chet-ok <65378320+Chet-ok@users.noreply.github.com>
2023-12-10 19:54:51 +0800

    Added basic initialization and dynamic check

---
+++ prac_06/programming_language.py
@@ -0,0 +1,24 @@
+"""
+CP1404/CP5632 Practical
+Programming language
+
+Estimated Time: 30 mins
+Actual Time:
+"""
+
+class ProgrammingLanguage:
+
+    def __int__(self,name,typing,reflection,year):
+        self.name = name.title()
+        self.typing = typing.title()
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        if self.typing == "Dynamic":
+            return True
+        else:
+            return False
+
+
+
