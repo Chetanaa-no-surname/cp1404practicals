8b9a9dac6b2959786d4bbc4ff0e9fbf280d9a976
Chet-ok <65378320+Chet-ok@users.noreply.github.com>
2023-12-10 20:07:43 +0800

    Completed Question

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -3,20 +3,25 @@ CP1404/CP5632 Practical
 Programming language
 
 Estimated Time: 30 mins
-Actual Time:
+Actual Time: 18 mins
 """
 
 
 class ProgrammingLanguage:
-
-    def __int__(self, name="", typing="", reflection="", year=0):
+    def __init__(self, name, typing, reflection, year):
+        """Initialising the Programming language"""
         self.name = name.title()
         self.typing = typing.title()
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
+        """Checking if langauge is dynamic"""
         if self.typing == "Dynamic":
             return True
         else:
             return False
+
+    def __str__(self):
+        """Printing all values of the language"""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
