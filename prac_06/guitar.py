f601b3c9c08bee59d63f57bb590bfdc8309e296f
Chet-ok <65378320+Chet-ok@users.noreply.github.com>
2023-12-10 21:00:22 +0800

    Added docstrings

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -10,22 +10,26 @@ Actual Time:  38 mins
 class Guitar:
 
     def __init__(self, name="", year=0, cost=0):
-        """ """
+        """Initialising Guitar variables """
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
+        """Printing of all of guitar's values"""
         return f"{self.name} ({self.year}): ${self.cost}"
 
     def get_age(self):
+        """Calculation of guitar's age"""
         CURRENT_YEAR = 2023
         age = CURRENT_YEAR - self.year
         return age
 
     def is_vintage(self):
+        OLD_AGE = 50
+        "Checking if guitar is over 50 years old"
         age = self.get_age()
-        if age >= 50:
+        if age >= OLD_AGE:
             return True
         else:
             return False
