7fb7ea3532020175f42fdcc1be3d6ff01339da09
Chet-ok <65378320+Chet-ok@users.noreply.github.com>
2023-12-10 21:00:51 +0800

    Changed code layout and added docstrings

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -6,21 +6,19 @@ from prac_06.guitar import Guitar
 
 
 def main():
-    guitar_names = []
+    guitar_names = [Guitar("Gibson L-5 CES", 1922, 16035.40), Guitar("Line 6 JTV-59", 2010, 1512.9)]
+
     print("My guitars!")
     name = input("Name: ")
-
-    guitar_names.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitar_names.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
     while name != "":
         year = int(input("Year: "))
         cost = float(input("Cost: $"))
         print(f"{name} ({year}) : ${cost:.2f} added.")
         guitar_names.append(Guitar(name, year, cost))
-        name = input("Name: ")
+        name = input("Name: ")  # Added to the bottom to ensure while loop checks for the blank input
 
-    print("\n"+"... snip ..."+"\n")
+    print("\n" + "... snip ..." + "\n")
 
     print("These are my guitars: ")
     count = 1
@@ -28,5 +26,6 @@ def main():
         vintage_string = " (vintage)" if guitar.is_vintage() == True else ""
         print(f"Guitar {count}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
         count += 1
+
 
 main()
