cf47cc1f56621915158c0d0fb61037044a1e6e55
Chet-ok <65378320+Chet-ok@users.noreply.github.com>
2023-12-11 18:04:26 +0800

    Completed at class

---
+++ prac_07/myguitars.py
@@ -0,0 +1,13 @@
+
+from prac_07.guitar import Guitar
+in_file = open("guitars.csv", "r")
+
+guitars = []
+
+for line in in_file:
+    guitar = (line.strip("\n")).split(",")
+    guitars.append(Guitar(guitar[0],guitar[1],guitar[2]))
+
+for guitar in guitars:
+    print(f"{guitar.name} was bought in {guitar.year} and costs {guitar.cost}")
+
