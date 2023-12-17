e343b061faee1832c858d497b7913a7793ac276f
Chet-ok <65378320+Chet-ok@users.noreply.github.com>
2023-12-17 18:26:33 +0800

    Made into functions

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,15 +1,26 @@
 from prac_07.guitar import Guitar
 
-in_file = open("guitars.csv", "r")
 
-guitars = []
+def main():
+    guitars = get_file_data("guitars")
+    print_data(guitars)
+    guitars = sorted(guitars)
+    print("\nResorted guitars list:\n")
+    print_data(guitars)
 
-for line in in_file:
-    guitar = (line.strip("\n")).split(",")
-    guitars.append(Guitar(guitar[0], guitar[1], guitar[2]))
 
-guitars = sorted(guitars)
+def get_file_data(name):
+    in_file = open(f"{name}.csv", "r")
+    guitars = []
+    for line in in_file:
+        guitar = (line.strip("\n")).split(",")
+        guitars.append(Guitar(guitar[0], guitar[1], guitar[2]))
+    return guitars
 
-for guitar in guitars:
-    print(f"{guitar.name} was bought in {guitar.year} and costs {guitar.cost}")
 
+def print_data(guitars):
+    for guitar in guitars:
+        print(f"{guitar.name} was bought in {guitar.year} and costs {guitar.cost}")
+
+
+main()
