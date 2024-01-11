f17bd56e9ecc4136032e9267298874b27bc9a4f2
Chet-ok <65378320+Chet-ok@users.noreply.github.com>
2024-01-11 16:53:53 +0800

    Completed part 1

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,13 @@
+"""
+CP1404/CP5632 Practical
+Car class
+"""
+
+from prac_09.taxi import Taxi
+
+my_taxi = Taxi("Prius 1", 100)  # Removed 1.23 since variable was added
+my_taxi.drive(40)
+print("Taxi details: " + str(my_taxi.name) + ", Current Fare: $" + str(my_taxi.get_fare()))
+my_taxi.start_fare()
+my_taxi.drive(100)
+print(my_taxi.__str__())
