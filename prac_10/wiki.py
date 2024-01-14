693dca785af8387ad944e0c1faa0f0e8b9657432
Chet-ok <65378320+Chet-ok@users.noreply.github.com>
2024-01-14 17:25:47 +0800

    Completed

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -3,21 +3,28 @@ import wikipedia
 search = input("Enter phrase to search: ")
 
 while search != "":
-    print("Search results: ")
-    print(wikipedia.search(search))
+    # print("Search results: ")
+    # print(wikipedia.search(search))
 
-    suggested = wikipedia.search(search,1)
+    suggested = wikipedia.search(search, 1)
 
     try:
-        print("Summary: ")
-        print(wikipedia.summary(suggested))
+        # print("Summary: ")
+        wikipedia.summary(suggested)
+        # print(wikipedia.summary(suggested))
     except wikipedia.exceptions.DisambiguationError as e:
+        print("Options: ")
         print(e.options)
-        new_search = input("Enter: ")
-        print("Summary: ")
-        print(wikipedia.summary(new_search))
+        suggested = input("Enter option choice: ")
+        # print("Summary: ")
+        # print(wikipedia.summary(suggested))
+        wikipedia.summary(suggested)
+
+    page_result = wikipedia.page(suggested,auto_suggest=False)  # Removed the printed outputs of the previous parts
+    print(page_result.title)
+    print(page_result.summary)
+    print(page_result.url)
 
     search = input("Enter phrase to search: ")
 
-print("Stopping")
-
+print("Ended :D")
